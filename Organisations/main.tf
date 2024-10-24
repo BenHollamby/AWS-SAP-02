34ae698f62383b3b9a931e41af8ffd1a639fd74b
Ben H <ben.hollamby@arkahna.io>
2024-10-25 10:35:16 +1300

    new dev account

--- Organisations/main.tf
+++ Organisations/main.tf
@@ -30,14 +30,6 @@ resource "aws_organizations_account" "my_account" {
 
   # Specify the parent OU for this account
   parent_id = aws_organizations_organizational_unit.gen_ou.id
-  
-  # Optional: Parent root or organizational unit for this account
-  # parent_id = aws_organizations_organization.org.roots[0].id
-  
-  # Optional: IAM user access to the new account
-  # role_name = "OrganizationAccountAccessRole"
-  
-  # Prevent destroying the account when running terraform destroy
   lifecycle {
     prevent_destroy = true
   }
@@ -46,20 +38,22 @@ resource "aws_organizations_account" "my_account" {
 resource "aws_organizations_account" "prod_account" {
   name  = var.prod_account_name
   email = var.prod_account_email
-
   # Specify the parent OU for this account
   parent_id = aws_organizations_organizational_unit.prod_ou.id
+}
+
+resource "aws_organizations_account" "dev_account" {
+  name  = var.dev_account_name
+  email = var.dev_account_email
+
+  # Specify the parent OU for this account
+  parent_id = aws_organizations_organizational_unit.dev_ou.id
+  role_name = "OrganizationAccountAccessRole"
+}
+
+output "devaccountid" {
+  value = aws_organizations_account.dev_account.id
   
-  # Optional: Parent root or organizational unit for this account
-  # parent_id = aws_organizations_organization.org.roots[0].id
-  
-  # Optional: IAM user access to the new account
-  # role_name = "OrganizationAccountAccessRole"
-  
-  # Prevent destroying the account when running terraform destroy
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_organizations_organizational_unit" "gen_ou" {
@@ -70,4 +64,29 @@ resource "aws_organizations_organizational_unit" "gen_ou" {
 resource "aws_organizations_organizational_unit" "prod_ou" {
   name      = "Production"
   parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "dev_ou" {
+  name      = "Development"  
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_iam_role" "assume_role" {
+  name = "OrganisationAccountAccessRole"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
--- Organisations/variables.tf
+++ Organisations/variables.tf
@@ -1,19 +1,29 @@
 variable "gen_account_name" {
-  description = "The name of the AWS account"
+  description = "The name of the general AWS account"
   type        = string
 }
 
 variable "gen_account_email" {
-  description = "The email of the AWS account"
+  description = "The email of the general AWS account"
   type        = string
 }
 
 variable "prod_account_name" {
-  description = "The name of the AWS account"
+  description = "The name of the prod AWS account"
   type        = string
 }
 
 variable "prod_account_email" {
-  description = "The email of the AWS account"
+  description = "The email of the prod AWS account"
+  type        = string
+}
+
+variable "dev_account_name" {
+  description = "The name of the dev AWS account"
+  type        = string
+}
+
+variable "dev_account_email" {
+  description = "The email of the dev AWS account"
   type        = string
 }
